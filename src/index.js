7ba7877899242f6627ce18c07218f9f696079484
mj <hi.minjungkim@gmail.com>
2020-05-16 09:53:12 +0900

    change to redux version, state mutation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETE_TODO"
+
+const reducer = (state =[], action) => {
+  console.log(action)
+  switch(action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }]
+    case DELETE_TODO:
+      return []
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()))
+
 const createToDo = toDo => {
   const li = document.createElement("li");
   li.innerText = toDo;
@@ -14,7 +33,7 @@ const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo)
+  store.dispatch({type: ADD_TODO, text: toDo});
 };
 
 form.addEventListener("submit", onSubmit);
