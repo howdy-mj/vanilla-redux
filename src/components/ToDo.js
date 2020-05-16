3a9012fc9e90e8935c04959949a9267fee3621f0
mj <hi.minjungkim@gmail.com>
2020-05-16 12:18:30 +0900

    Detail screen

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { actionCreators } from "../store";
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   );
 }
--- src/store.js
+++ src/store.js
@@ -4,35 +4,35 @@ const ADD = "ADD";
 const DELETE = "DELETE";
 
 const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
-    }
-}
+  return {
+    type: ADD,
+    text,
+  };
+};
 
 const deleteToDo = (id) => {
-    return {
-        type: DELETE,
-        id
-    }
-}
+  return {
+    type: DELETE,
+    id: parseInt(id),
+  };
+};
 
 const reducer = (state = [], action) => {
-    switch(action.type) {
-        case ADD:
-            return [{text: action.text, id: Date.now()}, ...state]
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
 
 const store = createStore(reducer);
 
 export const actionCreators = {
-    addToDo,
-    deleteToDo
-}
+  addToDo,
+  deleteToDo,
+};
 
 export default store;
